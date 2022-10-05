4525c5030f2b3a55aa753ee4f1c0b11d44c75c08
Enriconi <gabriel.enriconi@gmail.com>
2022-10-05 14:48:23 -0300

    ✨ 1005, 1006, 1020, 1035

---
+++ 1020.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int i;
+  int a, m, d;
+
+  cin >> i;
+
+  a = i / 365;
+  m = (i % 365) / 30;
+  d = (i % 365 % 30);
+
+  cout << a << " ano(s)" << endl;
+  cout << m << " mes(es)" << endl;
+  cout << d << " dia(s)" << endl;
+
+  return 0;
+}
---
+++ 1035.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  
+  bool condicao = b > c && d > a && c + d > a + b && a % 2 == 0 && c > 0 && d > 0;
+
+  if (condicao) cout << "Valores aceitos" << endl;
+  else cout << "Valores nao aceitos" << endl;
+
+  return 0;
+}
