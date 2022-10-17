4580fd18f0dd94a50b7099281f4df5c73f8e9435
Enriconi <gabriel.enriconi@gmail.com>
2022-10-16 23:41:01 -0300

    ✨ 1041, 1044

---
+++ 1041.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  float x, y;
+
+  cin >> x >> y;
+
+  if (x + y == 0) cout << "Origem" << endl;
+  if (y == 0 && x != 0) cout << "Eixo X" << endl;
+  if (x == 0 && y != 0) cout << "Eixo Y" << endl;
+  
+  if (x > 0 && y > 0) cout << "Q1" << endl;
+  if (x < 0 && y > 0) cout << "Q2" << endl;
+  if (x < 0 && y < 0) cout << "Q3" << endl;
+  if (x > 0 && y < 0) cout << "Q4" << endl;
+  
+  return 0;
+}
