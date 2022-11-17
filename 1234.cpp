8bfc6a00325bfbe9585904d717bf1a08263bed1f
Enriconi <gabriel.enriconi@gmail.com>
2022-11-17 17:14:38 -0300

    ✨ 1234

--- 1234.cpp
+++ 1234.cpp
@@ -8,16 +8,18 @@ int main()
   string n;
   int aux = 0;
 
-  getline(cin, n);
-
-  for (int i = 0; i < n.length(); i++) {
-    if (n[i] >= 65 && n[i] <= 90 || n[i] >= 97 && n[i] <= 122) aux++;
-
-    if (aux % 2 != 0) n[i] = toupper(n[i]);
-    else if (aux % 2 == 0) n[i] = tolower(n[i]);
+  while(getline(cin, n)) {
+    for (int i = 0; i < n.length(); i++) {
+      if (n[i] >= 65 && n[i] <= 90 || n[i] >= 97 && n[i] <= 122) aux++;
+  
+      if (aux % 2 != 0) n[i] = toupper(n[i]);
+      else if (aux % 2 == 0) n[i] = tolower(n[i]);
+    }
+  
+    cout << n << endl;
+    
+    aux = 0;
   }
 
-  cout << n << endl;
-  
   return 0;
 }
