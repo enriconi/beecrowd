f457105dea247771708a7df68ba0f3c1a8131f03
Enriconi <gabriel.enriconi@gmail.com>
2022-11-17 18:00:07 -0300

    ✨ 1241

---
+++ 1241.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+  int n;
+  char a[1001], b[1001];
+  bool isNotEqual = false;
+
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+    cin >> a >> b;
+
+    for (int j = strlen(a) - 1, k = strlen(b) - 1; k >= 0; j--, k--) {
+      if (a[j] != b[k]) isNotEqual = true;
+    }
+
+    if (isNotEqual) cout << "nao encaixa" << endl;
+    else cout << "encaixa" << endl;
+
+    isNotEqual = false;
+  }
+
+  return 0;
+}
