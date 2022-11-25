ec13d4b451156336f6c0e2d45b6e23015429a7df
Enriconi <gabriel.enriconi@gmail.com>
2022-11-24 23:36:22 -0300

    🚑 1120

--- 1120.cpp
+++ 1120.cpp
@@ -1,33 +1,42 @@
 #include <iostream>
 #include <cstring>
+#include <algorithm>
 
 using namespace std;
+
+string removeZero(string n)
+{
+    int i = 0;
+  
+    while (n[i] == '0') i++;
+    n.erase(0, i);
+  
+    return n;
+}
 
 int main()
 {
   char a;
   string b;
-  bool isEmpty = false, isEqual = true;
+  bool isEqual = true, isOnlyZero = true;
 
   while (cin >> a >> b && (a != '0' || b != "0")) {
-    for (int i = 0; i < b.length(); i++) {
+    b.erase(remove(b.begin(), b.end(), a), b.end());
 
-      for (int j = 0; j < b.length(); j++) {
-        if (b[j] != a) isEqual = false;
+    for (int j = 0; j < b.length(); j++) {
+      if (b[j] != '0') {
+        isOnlyZero = false;
       }
-      
-      if (b[i] == '0' || isEqual) {
-        cout << "0";
-        break;
-      }
-
-      else if (b[i] != a) cout << b[i];
     }
 
-    cout << endl;
+    if (isOnlyZero) {
+      cout << "0" << endl;
+    } else {      
+      string c = removeZero(b);
+      cout << c << endl;
+    }
 
-    isEmpty = false;
-    isEqual = true;
+    isOnlyZero = true;
   }
 
   return 0;
