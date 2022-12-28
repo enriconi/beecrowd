248b2c2e661323a09647ab328a060726798b66ee
Enriconi <gabriel.enriconi@gmail.com>
2022-12-27 23:28:50 -0300

    ✨ 1471

---
+++ 1471.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  int m, n, a;
+
+  while (cin >> m >> n) {
+    vector<int> v;
+    bool b = true;
+  
+    for (int i = 0; i < n; i++) {
+      cin >> a;
+      v.push_back(a);
+    }
+
+    for (int i = 1; i <= m; i++) {
+      if (!(count(v.begin(), v.end(), i))) {
+        cout << i << " ";
+        b = false;
+      }
+    }
+
+    if (b) cout << "*";
+    cout << endl;
+  }
+
+
+  return 0;
+}
