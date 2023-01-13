33f607d199d594bb84e3e7746b1069f38b7872b3
Enriconi <gabriel.enriconi@gmail.com>
2023-01-12 23:43:23 -0300

    ✨ 1847

---
+++ 1847.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a, b, c;
+
+  cin >> a >> b >> c;
+
+  if (a > b && c >= b) cout << ":)" << endl;
+  else if (b > a && b >= c) cout << ":(" << endl;
+  else if (b > a && c > b && ((b - a) > (c - b))) cout << ":(" << endl;
+  else if (b > a && c > b && ((c - b) >= (b - a))) cout << ":)" << endl;
+  else if (a > b && b > c && ((a - b) > (b - c))) cout << ":)" << endl;
+  else if (a > b && b > c && ((b - c) >= (a - b))) cout << ":(" << endl;
+  else if (a == b) {
+    if (c > b) cout << ":)" << endl;
+    else cout << ":(" << endl;
+  }
+  
+  
+  return 0;
+}
