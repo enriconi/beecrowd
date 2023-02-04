f0997705988fd860433c2f5049b30f82894d865b
Enriconi <gabriel.enriconi@gmail.com>
2023-02-03 21:28:37 -0300

    ✨ 1185

---
+++ 1185.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+
+  double m[12][12], sum = 0.0;
+  char c;
+
+  cin >> c;
+
+  for (int i = 0; i < 12; i++) {
+    for (int j = 0; j < 12; j++) {
+      cin >> m[i][j];
+      if (j < (11 - i)) sum += m[i][j];
+    }
+  }
+
+  cout << fixed << setprecision(1);
+  cout << (c == 'S' ? sum : sum / 66.0) << endl;
+  
+  return 0;
+}
