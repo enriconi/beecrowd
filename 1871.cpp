0f9eb524697d30e068520425786c267ce94da5b0
Enriconi <gabriel.enriconi@gmail.com>
2023-02-17 19:59:13 -0300

    ✨ 1871

---
+++ 1871.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  string n, m;
+
+  while (cin >> n >> m && (n != "0" && m != "0")) {
+    unsigned long long aux = (stoi(n) + stoi(m));
+    int size = trunc(log10(aux)) + 1;
+    string sum = to_string(aux);
+
+    for (int i = 0; i < size; i++) {
+      if (sum[i] != '\0' && sum[i] != '0') cout << sum[i];
+    }
+
+    cout << endl;
+  }
+
+  return 0;
+}
