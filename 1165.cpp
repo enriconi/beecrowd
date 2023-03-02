8b90bb7bc137c23805caabb8a7e1cf1965d88a22
Enriconi <gabriel.enriconi@gmail.com>
2023-03-02 01:25:39 -0300

    ✨ 1165

---
+++ 1165.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+static bool isPrime(int n) {
+  if (n <= 1) return false;
+  
+  for (int i = 2; i < n; i++) {
+    if (n % i == 0 && i != n) return false;
+  }
+  
+  return true;
+}
+
+int main()
+{
+  int n, number;
+
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+    cin >> number;
+    
+    if (isPrime(number)) cout << number << " eh primo" << endl;
+    else cout << number << " nao eh primo" << endl;
+  }
+  
+  return 0;
+}
