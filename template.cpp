4dfc0333acf1f363b1509d3a24a88e9c1705c4e2
Enriconi <gabriel.enriconi@gmail.com>
2023-09-22 04:10:37 -0300

    ✨ Adicionando template mais atual

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAXN 50010
+#define endl '\n'
+
+typedef long long ll;
+
+const int INF = 1000000010;
+
+int main() {
+  ios_base::sync_with_stdio(0); cin.tie(0);
+  return 0;
+}
